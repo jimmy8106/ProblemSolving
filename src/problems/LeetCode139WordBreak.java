10d451c3a1b88347642952ea52a7a417309b87f2
jimmy8106 <loktar8106@gmail.com>
2023-05-05 14:54:39 +0800

    完成dp作法

--- src/problems/LeetCode139WordBreak.java
+++ src/problems/LeetCode139WordBreak.java
@@ -5,23 +5,32 @@ import java.util.Iterator;
 import java.util.List;
 
 public class LeetCode139WordBreak {
-	public static void main(String[] args) {
-		int[] nums = {10,9,2,5,3,7,101,18};
-		
-	}
-	
-	private static boolean wordBreak(String s, List<String> wordDict)  {
-		boolean[] dp = new boolean[wordDict.size()+1];
-		dp[0] = true;//n+1, and a true for index 0 array
-		for (int i = 1; i < dp.length; i++) {
-			for (int j = 0; j < dp.length; j++) {
-				if (dp[j] && wordDict.contains(s.substring(j,i))){
-					dp[i] = true;
-					break;
-				}
-			}
-		}
-		
-		return dp[wordDict.size()-1];
+  public static void main(String[] args) {
+    String[] wordDict = { "leet", "code" };
+    String s = "leetcode";
+    System.out.println(wordBreak(s, Arrays.asList(wordDict)));
+  }
+
+  private static boolean wordBreak(String s, List<String> wordDict) {
+    boolean[] dp = new boolean[s.length() + 1];
+    dp[0] = true;// n+1, and a true for index 0 array
+    for (int i = 0; i < dp.length; i++) {
+      if(!dp[i]) {
+        continue;
+      }
+      //第一次會檢查 i到word字串長的值是否有相同的字
+      //有個話將dp[i到word字串長的值]改為true
+      for (String word : wordDict) {
+        int subStringLen = i + word.length();
+        //超出界線 換下一個
+        if(subStringLen>s.length()) {
+          continue;
+        }
+        if(s.substring(i, subStringLen).equals(word))
+          dp[subStringLen] = true;
+      }
     }
+
+    return dp[s.length()];
+  }
 }
